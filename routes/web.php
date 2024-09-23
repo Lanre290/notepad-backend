d3585c802213e4df5295acf7d37b58eea1f49379
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-09-23 14:03:57 +0100

    fixinf jwt auth

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,20 @@ use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 
-Route::get('/jwt', function () {
-    // $credentials = $request->only('email', 'password');
-    // $credentials = ['email' => , 'password' => '20838'];
+// Route::get('/jwt', function () {
+//     // $credentials = $request->only('email', 'password');
+//     // $credentials = ['email' => , 'password' => '20838'];
 
-    $email = 'lanre2967@gmail.com';
-    $refreshPayload = [
-        'sub' => $email,
-        'exp' => now()->addDays(7)->timestamp, 
-    ];
+//     $email = 'lanre2967@gmail.com';
+//     $refreshPayload = [
+//         'sub' => $email,
+//         'exp' => now()->addDays(7)->timestamp, 
+//     ];
 
-    $refreshToken = JWT::encode($refreshPayload, env('JWT_SECRET'), 'HS256');
+//     $refreshToken = JWT::encode($refreshPayload, env('JWT_SECRET'), 'HS256');
 
-    return response()->json(['token' => $refreshToken]);
-});
+//     return response()->json(['token' => $refreshToken]);
+// });
 
-Route::get('/check', function () {
-    try {
-        $decoded = JWT::decode('fef.fefe.fef', new Key(env('JWT_SECRET'), 'HS256')); // Use your JWT secret
-        // If needed, check additional claims or user info here
-        return response()->json(['decoded' => $decoded]);
-    } catch (Exception $e) {
-        return response()->json(['error' => 'invalid_token'], 401);
-    }
-});
 
 // Route::get('protected-route', [YourController::class, 'yourMethod'])->middleware('validate.token');
