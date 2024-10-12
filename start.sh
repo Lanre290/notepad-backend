1b797254472dde5e67e1b890f45601d722dc25d4
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-10-12 11:25:04 +0100

    deploy fixes

--- start.sh
+++ start.sh
@@ -4,3 +4,7 @@ nginx -g 'daemon off;'
 # Any setup commands can go here
 php artisan migrate --force   # Example: Run database migrations
 php-fpm                       # Start the PHP FastCGI Process Manager
+composer install --no-dev --optimize-autoloader
+php artisan key:generate
+npm install
+npm run prod
